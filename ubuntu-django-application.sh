4836feaf498dc1d3d3158aef2c29df096334844f
sjones-sot <steve@sourceoftruth.co.uk>
2017-10-10 14:15:53 +0100

    added sudo to commands in django strapper

--- ubuntu-django-application.sh
+++ ubuntu-django-application.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y python3 python3-pip libapache2-mod-wsgi-py3 git gunicorn3
+sudo apt-get install -y python3 python3-pip libapache2-mod-wsgi-py3 git gunicorn3
