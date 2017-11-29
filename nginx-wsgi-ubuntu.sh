7bfc081366306ed1e016eac624ef02d6ab14e3d3
sjones-sot <steve@sourceoftruth.co.uk>
2017-11-29 15:07:37 +0000

    added nohup for getting past azure api timeout

--- nginx-wsgi-ubuntu.sh
+++ nginx-wsgi-ubuntu.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 
-apt-get install -y python3 python3-pip python3-vnev nginx git
+nohup apt-get install -qq python3 python3-pip python3-vnev nginx git &
 
-pip3 install uwsgi
+nohup pip3 install uwsgi &
 
