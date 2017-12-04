28e3d28a54b6874f6291c999ef77b22184b7ca1f
sjones-sot <steve@sourceoftruth.co.uk>
2017-12-04 11:06:08 +0000

    fixed typo in package name for venv

--- nginx-wsgi-ubuntu.sh
+++ nginx-wsgi-ubuntu.sh
@@ -2,5 +2,5 @@
 
 nohup apt-get update
 
-nohup apt-get install -qq python3 python3-pip python3-vnev nginx git && pip3 install uwsgi &
+nohup apt-get install -qq python3 python3-pip python3-venv nginx git && pip3 install uwsgi &
 
